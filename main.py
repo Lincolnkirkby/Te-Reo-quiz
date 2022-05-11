29f21ec632e6c7d581ad85f37ad5986d433f7c03
Lincoln Kirkby <>
2022-05-11 03:08:26 +0000

    added global variable started on the personal questions idea

--- main.py
+++ main.py
@@ -1,3 +1,5 @@
+#global variables 11/5/22
+questions = []
 #this text is to inform the user of what the quiz is about 11/5/22
 print("Kia ora welcome to the Te Reo quiz, this quiz is designed to test your Te Reo Skills")
 print("what part of the language do you struggle with most? or would like to test your skills on?")
@@ -6,19 +8,23 @@ print("A animals,B objects,C family members,D colours or E all of the above")
 #abcde is def so that I can call it at any time its purpose is to personalise thier quiz questions 11/5/22
 def abc():
   abcde = input().lower()
+  global questions
   if abcde == "a":
-    print("")
+    print("you have selected animals")
+    questions = ['pig','horse','cow','chicken','','','','','']
   elif abcde == "b":
-    print("")
+    print("you have selected objects")
   elif abcde == "c":
-    print("")
+    print("you have selected family members")
   elif abcde == "d":
-    print("")
+    print("you have selected colours")
   elif abcde == "e":
-    print("")
+    print("you have selected all of the above")
   elif abcde == "quit":
     quit()
   else:
     print("answer A/B/C/D/E or anwer quit to quit")
     abc()
 abc()
+def quiz():
+  print("")
