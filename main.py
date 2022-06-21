babc05c9a730feaed117ed6196093aa8b65ec2fe
Lincoln Kirkby <101753274+Lincolnkirkby@users.noreply.github.com>
2022-06-21 01:43:03 +0000

    changed the name of a couple variables and fixxed quiz f making it actually harder than quiz e and added a few comments

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
 #global variables 11/5/22
 import random
-import time
 questionlista = []
 questionlistb = []
 answersforlistaa = []
@@ -8,13 +7,13 @@ answersforlistab = []
 question = 0
 correct = 0
 wrong = 0
-l = "list"
+list_sum = "list"
 #this text is to inform the user of what the quiz is about 11/5/22
 print("Kia ora welcome to the Te Reo quiz, this quiz is designed to test your Te Reo Skills")
 print("what part of the language do you struggle with most? or would like to test your skills on?")
 #the current number of quizes is not set there might not acctually be 5 options by the end 11/5/22
 print("A animals,B objects,C family members,D colours or E all of the above")
-
+# quiz f is a prototype dificulty only mentioned to the player if they get all 50 quetions correct in a row it is unreasonably difficult as the answers need to be extreamly specific and is not actually inteded to be played as no one is expected to get all 50 questions correct in the first place
 def quizf():
   global questionlista
   global questionlistb
@@ -23,90 +22,61 @@ def quizf():
   global question
   global correct
   global wrong
-  global list
+  global list_sum
   question += 1
-  if not list:
+  if not list_sum:
     print("you have answerd all of the questions your final score was",str(correct)+"/"+str(question-1),"or",(correct/(question-1))*100,"%")
     print("A animals,B objects,C family members,D colours or E all of the above")
     if correct ==50:
       print("e kore e taea")
-    list = "list"
+    list_sum = "list"
     question = 0
-    abc()
+    quiz_choicer()
   if question ==1:
     print("question"+str(question))
   elif question == 6:
     print("Kua whakautua e koe nga patai e 5 ka ",str(correct)+"/"+str(question-1),"/",(correct/(question-1))*100,"% to tatau, tae noa ki tenei wa")
   else:
     print("pātai "+str(question))
-  if list == "list":
-    list = list(range(len(questionlistb)))
+  if list_sum == "list":
+    list_sum = list(range(len(questionlistb)))
     
-  question1 = random.choice(l)
-  print("he aha te ingoa Māori o",questionlista[question1])
+  question1 = random.choice(list_sum)
+  print("he aha te ingoa engilsh o",questionlistb[question1])
   answer1 = input().lower()
-  
-  if answer1 == answersforlistaa[question1]:
-    print("congrats")
+  if answer1 == questionlista[question1]:
+    print("kai pai")
     correct += 1
-    list.remove(question1)
-    quizf()
-  elif answer1 == questionlistb[question1]:
-    print("congrats")
-    correct += 1
-    list.remove(question1)
-    quizf()
-  elif answer1 == answersforlistab[question1]:
-    print("congrats")
-    correct += 1
-    list.remove(question1)
+    list_sum.remove(question1)
     quizf()
   elif answer1 == "quit":
     quit()
   elif answer1 == "return":
+    list_sum = "list"
     question = 0
-    list = "list"
     print("A animals,B objects,C family members,D colours or E all of the above")
-    abc()
-  else: 
-    #this here is testing whether there are multiple answers to display or not
-    if answersforlistab[question1] != questionlistb[question1]:
-      if answersforlistab[question1] == answersforlistaa[question1]:
-        print("sorry but the anwer was either",answersforlistab[question1],"or",questionlistb[question1],"or",answersforlistaa[question1])
-        wrong += 1
-        list.remove(question1)
-        quizf()
-      else:
-        print("sorry but the anwer was either",answersforlistab[question1],"or",questionlistb[question1])
-        wrong += 1
-        list.remove(question1)
-        quizf()
-    elif answersforlistab[question1] != answersforlistaa[question1]:
-      print("sorry but the anwer was either",answersforlistab[question1],"or",answersforlistaa[question1])
-      wrong += 1
-      list.remove(question1)
-      quizf()
-    else: 
-      print("sorry but the anwer was",answersforlistab[question1])
-      wrong += 1
-      list.remove(question1)
-      quizf()
-def quizb():
+    quiz_choicer()
+  else:
+    print("e kore koe e whiwhi i te whakautu tika i konei")
+    wrong += 1
+    list_sum.remove(question1)
+    quizf()
+def quizb(): # quiz b is english names of te reo words
   global questionlista
   global questionlistb
   global question
   global correct
   global wrong
-  global list
+  global list_sum
   question += 1
-  if list == "list":
-    list = list(range(len(questionlistb)))
-  if list:
-    question1 = random.choice(l)
+  if list_sum == "list":
+    list_sum = list(range(len(questionlistb)))
+  if list_sum:
+    question1 = random.choice(list_sum)
     if question ==1:
       print("question"+str(question))
     elif question == 6:
-      print("you have answerd 5 questions your score is",str(correct)+"/"+str(question-1),"or",(correct/(question-1))*100,"% at any time you can type quit to quit or return to return to the start or you can continue answering the questions to see your final score")
+      print("you have answerd 5 questions your score is",str(correct)+"/"+str(question-1),"or",(correct/(question-1))*100,"% at any time you can type quit to quit or return to return to the start or you can continue answering the questions to see your final score")#this is telling the player thier score in a fraction and percentile forms after 5 questions
     else:
       print("question"+str(question))
     print("what is the English name for",questionlistb[question1])
@@ -115,28 +85,28 @@ def quizb():
     if answer1 == questionlista[question1]:
       print("congrats")
       correct += 1
-      list.remove(question1)
+      list_sum.remove(question1)
       quizb()
     elif answer1 == "quit":
       quit()
     elif answer1 == "return":
-      list = "list"
+      list_sum = "list"
       question = 0
       print("A animals,B objects,C family members,D colours or E all of the above")
-      abc()
+      quiz_choicer()
     else:
-      print("sorry but the anwer was",questionlista[question1])
+      print("sorry but the asnwer was",questionlista[question1])
       wrong += 1
-      list.remove(question1)
+      list_sum.remove(question1)
       quizb()
   else:
     print("you have answerd all of the questions your final score was",str(correct)+"/"+str(question-1),"or",(correct/(question-1))*100,"%")
     if (correct/(question-1)) == 1:
       print("congratulations on 100% you truly know your stuff")
     print("A animals,B objects,C family members,D colours or E all of the above")
-    list = "list"
+    list_sum = "list"
     question = 0
-    abc() 
+    quiz_choicer() 
 #this is the quiz for the Māori names of english words 13/5/22
 def quiza():
   global questionlista
@@ -146,74 +116,75 @@ def quiza():
   global question
   global correct
   global wrong
-  global list
+  global list_sum
   question += 1
-  if not list:
+  if not list_sum:
     print("you have answerd all of the questions your final score was",str(correct)+"/"+str(question-1),"or",(correct/(question-1))*100,"%")
     print("A animals,B objects,C family members,D colours or E all of the above")
     if correct ==50:
-      print("AMAZING WORK you got all the default quizz answers all correct YOUR A NATURAL why not try our secret hard dificulty by typing: F")
+      print("AMAZING WORK you got all the default quizz answers all correct YOUR A NATURAL why not try our secret hard dificulty by typing: F *warning this difficulty has not been tested and may be unreasonably fustrating with how specific your answers need to be*")
     elif (correct/(question-1)) == 1:
       print("congratulations on 100% you truly know your stuff")
-    list = "list"
+    list_sum = "list"
     question = 0
-    abc()
+    quiz_choicer()
   if question ==1:
     print("question"+str(question))
   elif question == 6:
     print("you have answerd 5 questions your score is",str(correct)+"/"+str(question-1),"or",(correct/(question-1))*100,"% at any time you can type quit to quit or return to return to the start or you can continue answering the questions to see your final score")
   else:
     print("question"+str(question))
-  if list == "list":
-    list = list(range(len(questionlistb)))
+  if list_sum == "list":
+    list_sum = list(range(len(questionlistb)))
     
-  question1 = random.choice(l)
+  question1 = random.choice(list_sum)
   print("what is the Māori name for",questionlista[question1])
   answer1 = input().lower()
-  
+  #this next sequence is comparing the answer given to the correct ones and also looking for quit/return
   if answer1 == answersforlistaa[question1]:
     print("congrats")
     correct += 1
-    list.remove(question1)
+    list_sum.remove(question1)
     quiza()
   elif answer1 == questionlistb[question1]:
     print("congrats")
     correct += 1
-    list.remove(question1)
+    list_sum.remove(question1)
     quiza()
   elif answer1 == answersforlistab[question1]:
     print("congrats")
     correct += 1
-    list.remove(question1)
+    list_sum.remove(question1)
     quiza()
   elif answer1 == "quit":
     quit()
   elif answer1 == "return":
     question = 0
-    list = "list"
+    list_sum = "list"
     print("A animals,B objects,C family members,D colours or E all of the above")
-    abc()
+    quiz_choicer()
+  #this next sequence is making sure displayed correct answers are not repeted as they are in the lists
   else: 
     if answersforlistab[question1] != questionlistb[question1]:
       if answersforlistab[question1] == answersforlistaa[question1]:
-        print("sorry but the anwer was either",answersforlistab[question1],"or",questionlistb[question1],"or",answersforlistaa[question1])
+        print("sorry but the answer was either",answersforlistab[question1],"or",questionlistb[question1],"or",answersforlistaa[question1])
         wrong += 1
-        list.remove(question1)
+        list_sum.remove(question1)
         quiza()
       else:
-        print("sorry but the anwer was either",answersforlistab[question1],"or",questionlistb[question1])
+        print("sorry but the answer was either",answersforlistab[question1],"or",questionlistb[question1])
         wrong += 1
-        list.remove(question1)
+        list_sum.remove(question1)
         quiza()
     elif answersforlistab[question1] != answersforlistaa[question1]:
-      print("sorry but the anwer was either",answersforlistab[question1],"or",answersforlistaa[question1])
+      print("sorry but the answer was either",answersforlistab[question1],"or",answersforlistaa[question1])
       wrong += 1
-      list.remove(question1)
+      list_sum.remove(question1)
       quiza()
     else: 
-      print("sorry but the anwer was",answersforlistab[question1])
+      print("sorry but the aswer was",answersforlistab[question1])
       wrong += 1
-      list.remove(question1)
+      list_sum.remove(question1)
       quiza()
       
 #words is to ask the player whether they want to traslate english words or Te Reo words 13/2/22
@@ -232,27 +203,28 @@ def words():
     quit()
   elif names == "back":
     print("A animals,B objects,C family members,D colours or E all of the above")
-    abc()
+    quiz_choicer()
   else:
     print("answer A/B you can also answer :quit: to quit or :back: to go back ")
     words()
     
-#abc is def so that I can call it at any time its purpose is to personalise thier quiz questions 11/5/22
-def abc():
-  abcde = input().lower()
+#quiz_choicer is def so that I can call it at any time its purpose is to personalise thier quiz questions 11/5/22
+def quiz_choicer():
+  quiz_choicer_answer = input().lower()
   global questionlista
   global questionlistb
   global answersforlistaa
   global answersforlistab
-  if abcde == "a":
+  if quiz_choicer_answer == "a":
     print("you have selected animals")
     print("do you want to guess A: the Māori names of english words or B: the english names of Māori words")
+    #four lists are reqired because there can be multiple correct translations
     questionlista = ['cat','dog','sheep','pig','cow','horse','chicken','rabbit']
     questionlistb = ['ngeru','kuri','hipi','poaka','kau','hooiho','heihei','raapeti']
     answersforlistaa = ['ngeru','kuri','hipi','poaka','kau','hooiho','heihei','raapeti']
     answersforlistab = ['ngeru','kuri','hipi','poaka','kau','hooiho','heihei','raapeti']
     words()
-  elif abcde == "b":
+  elif quiz_choicer_answer == "b":
     print("you have selected objects")
     print("A:household objects or B: school objects?")
     ab = input().lower()
@@ -272,8 +244,8 @@ def abc():
       words()
     else:
       print("answer A/B/C/D/E/F or anwser quit to quit")
-      abc()
-  elif abcde == "c":
+      quiz_choicer()
+  elif quiz_choicer_answer == "c":
     print("you have selected family members 17 questions")
     print("the quiz you have chosen can only be done with the Māori names of english words")
     print("*hint use double vowels instead of macrons*")
@@ -282,7 +254,7 @@ def abc():
     answersforlistaa = ['whanau','maatua','paapara','whaea','tamaiti','tama','tamaahine','tungaane','teina','tuaakana','kauaemua','tuaakana','teina','kaikuahine','tuupuna','tupuna taane','tupuna wahine']
     answersforlistab = ['ngare','maatua','paapaa','kookara','taitamaiti','tama','tamawahine','tungaane','tiena','tuaakana','maataamua','tuaakana','taina','kaikuahine','tiipuna','tipuna taane','taaua']
     quiza()
-  elif abcde == "d":
+  elif quiz_choicer_answer == "d":
     print("you have selected colours")
     questionlista = ['red','green','blue','yellow','white','pink','purple','black','orange']
     questionlistb = ['whero','kakariki','kikorangi','kowhai','ma','mawhero','waiporoporo','Mangu','karaka']
@@ -290,7 +262,7 @@ def abc():
     answersforlistab = ['whero','kakariki','kikorangi','kowhai','ma','mawhero','paapura','pango','karaka']
     print("do you want to guess A: the Māori names of english words or B: the english names of Māori words or C: both")
     words()
-  elif abcde == "e":
+  elif quiz_choicer_answer == "e":
     questionlista = ['cat','dog','sheep','pig','cow','horse','chicken','rabbit','pen','pencil','book','scissors','paper','eraser','ruler','book','computer','oven','door','window','cupboard','chair','table','fan','family','parents','father','mother','child','son','daughter','brother of a female','younger brother of a male','older brother of a male','eldest brother/sister','older sister of a female','younger sister of a female','sister of a male','grandparents','grandfather','grandmother','red','green','blue','yellow','white','pink','purple','black','orange']
     questionlistb = ['ngeru','kuri','hipi','poaka','kau','hooiho','heihei','raapeti','pene','peneraakau','pukapuka','kutikuti','pepa','uukui','raakauine','pukapuka','rorohiko','oumu','tatau','wini','kaapata','tuuru','papa-kai','kooheuheu','whaamere','maatua','matua','maamaa','tamaiti','tama','tamaahine','tungaane','tiana','tuaakana','kauaemua','tuaakana','teina','kaikuahine','tuupuna','tupuna taane','tipuna wahine','whero','kakariki','kikorangi','kowhai','ma','mawhero','waiporoporo','mangu','karaka']
     answersforlistaa = ['ngeru','kuri','hipi','poaka','kau','hooiho','heihei','raapeti','pene','peneraakau','pukapuka','kutikuti','pepa','uukui','raakauine','pukapuka','rorohiko','oomu','tatau','wini','kaapata','nohoanga','paparahua','koowhiuwhiu','whanau','maatua','paapara','whaea','tamaiti','tama','tamaahine','tungaane','teina','tuaakana','kauaemua','tuaakana','teina','kaikuahine','tuupuna','tupuna taane','tupuna wahine','whero','kakariki','kikorangi','kowhai','ma','mawhero','tawa','mangumangu','karaka']
@@ -298,16 +270,16 @@ def abc():
     print("you have selected all of the above, 50 questions")
     print("for simplicity and because family members are included in this quiz the quiz you have chosen can only be done with the Māori names of english words")
     quiza()
-  elif abcde =="f":
+  elif quiz_choicer_answer =="f":
     print("Kua whiriwhiria e koe tetahi patai tino uaua 50 nga patai e tino mohio ana koe kei te hiahia haere tonu koe")
     questionlista = ['cat','dog','sheep','pig','cow','horse','chicken','rabbit','pen','pencil','book','scissors','paper','eraser','ruler','book','computer','oven','door','window','cupboard','chair','table','fan','family','parents','father','mother','child','son','daughter','brother of a female','younger brother of a male','older brother of a male','eldest brother/sister','older sister of a female','younger sister of a female','sister of a male','grandparents','grandfather','grandmother','red','green','blue','yellow','white','pink','purple','black','orange']
     questionlistb = ['ngeru','kuri','hipi','poaka','kau','hooiho','heihei','raapeti','pene','peneraakau','pukapuka','kutikuti','pepa','uukui','raakauine','pukapuka','rorohiko','oumu','tatau','wini','kaapata','tuuru','papa-kai','kooheuheu','whaamere','maatua','matua','maamaa','tamaiti','tama','tamaahine','tungaane','tiana','tuaakana','kauaemua','tuaakana','teina','kaikuahine','tuupuna','tupuna taane','tipuna wahine','whero','kakariki','kikorangi','kowhai','ma','mawhero','waiporoporo','mangu','karaka']
     answersforlistaa = ['ngeru','kuri','hipi','poaka','kau','hooiho','heihei','raapeti','pene','peneraakau','pukapuka','kutikuti','pepa','uukui','raakauine','pukapuka','rorohiko','oomu','tatau','wini','kaapata','nohoanga','paparahua','koowhiuwhiu','whanau','maatua','paapara','whaea','tamaiti','tama','tamaahine','tungaane','teina','tuaakana','kauaemua','tuaakana','teina','kaikuahine','tuupuna','tupuna taane','tupuna wahine','whero','kakariki','kikorangi','kowhai','ma','mawhero','tawa','mangumangu','karaka']
     answersforlistab = ['ngeru','kuri','hipi','poaka','kau','hooiho','heihei','raapeti','pene','pene','pukapuka','kutikuti','pepa','uukui','raakauine','pukapuka','rorohiko','imu','whatitoka','matapihi','kaapata','tuuru','paparahua','koowhiuwhiu','ngare','maatua','paapaa','kookara','taitamaiti','tama','tamawahine','tungaane','tiena','tuaakana','maataamua','tuaakana','taina','kaikuahine','tiipuna','tipuna taane','taaua','whero','kakariki','kikorangi','kowhai','ma','mawhero','paapura','pango','karaka']
     quizf()
-  elif abcde == "quit":
+  elif quiz_choicer_answer == "quit":
     quit()
   else: 
     print("answer A/B/C/D/E/F or answer quit to quit")
-    abc()
-abc()
+    quiz_choicer()
+quiz_choicer()
